d042cc68142731954ec116aeca29317825692084
Hazem Soliman <hazemsoliman0813@gmail.com>
2024-12-02 06:43:03 +0200

    remove unused parameter

--- src/linux/parse-string.pipe.ts
+++ src/linux/parse-string.pipe.ts
@@ -2,7 +2,7 @@ import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
 
 @Injectable()
 export class ParseStringPipe implements PipeTransform {
-  transform(value: any, metadata: ArgumentMetadata) {
+  transform(value: any) {
     if(typeof(value) !== 'string') {
       return Error(`validation failed, expected string but got ${typeof(value)}`)
     }
